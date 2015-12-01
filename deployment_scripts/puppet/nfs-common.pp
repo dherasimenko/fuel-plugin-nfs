8b16ca79fd9ce316917fd269c6fcedb65adf15a2
Dima Herasimenko <dherasimenko@mirantis.com>
2015-12-01 18:21:20 +0200

    auto build of nfs server, auto mount of nfs server to compute

--- deployment_scripts/puppet/nfs-common.pp
+++ deployment_scripts/puppet/nfs-common.pp
@@ -1,17 +1,34 @@
 notice('MODULAR: nfs-common.pp')
 
-    if $::osfamily == 'Debian' {
-      $required_pkgs = [ 'rpcbind', 'nfs-common' ]
-      $service_name = 'rpcbind'
+# get storage IP of NFS Servers, mount storaget on Compute node
+define nfs_server_ip {
+  if $name['role'] == 'nfs-server' {
+    mount { '/mnt/nfs':
+      ensure  => mounted,
+      atboot  => true,
+      device  => "${name['storage_address']}:/nfs",
+      fstype  => 'nfs',
+      options => 'vers=4',
+    }
+  }
+}
 
-      package { $required_pkgs:
-        ensure => present,
-      }
-    
-      service { $service_name:
-        ensure => running,
-      }
-    }
-    else {
-      fail("Unsuported osfamily ${::osfamily}, currently Debian are the only supported platforms")
-    }
+if $::osfamily == 'Debian' {
+  $required_pkgs = [ 'rpcbind', 'nfs-common' ]
+  $service_name = 'rpcbind'
+  
+  package { $required_pkgs:
+    ensure => present,
+  }
+  
+  service { $service_name:
+    ensure => running,
+  }
+
+  $nodes = hiera('nodes')
+  nfs_server_ip { $nodes: }
+
+}
+else {
+  fail("Unsuported osfamily ${::osfamily}, currently Debian are the only supported platforms")
+}
--- deployment_scripts/puppet/nfs-server.pp
+++ deployment_scripts/puppet/nfs-server.pp
@@ -1,33 +1,33 @@
 notice('MODULAR: nfs-server.pp')
 
-    if $::osfamily == 'Debian' {
-      $required_pkgs = [ 'rpcbind', 'nfs-kernel-server' ]
-      $services_name = 'nfs-kernel-server'
-      $nfs_srv_folder = '/nfs'
+if $::osfamily == 'Debian' {
+  $required_pkgs = [ 'rpcbind', 'nfs-kernel-server' ]
+  $services_name = 'nfs-kernel-server'
+  $nfs_srv_folder = '/nfs'
 
-      package { $required_pkgs:
-        ensure => present,
-      }
-
-      file { $nfs_srv_folder:
-        ensure => 'directory',
-        mode   => '0777',
-      }
-
-      file { '/etc/exports':
-        content => "${nfs_srv_folder} ${::network_br_storage}/255.255.255.0 (rw,sync,no_subtree_check)",
-      }
-
-      exec { 'exportfs -ra':
-        path        => ['/usr/bin', '/usr/sbin'],
-        subscribe   => File['/etc/exports'],
-        refreshonly => true
-      }
-    
-      service { $services_name:
-        ensure => running,
-      }
-    }
-    else {
-      fail("Unsuported osfamily ${::osfamily}, currently Debian are the only supported platforms")
-    }
+  package { $required_pkgs:
+    ensure => present,
+  }
+  
+  file { $nfs_srv_folder:
+    ensure => 'directory',
+    mode   => '0777',
+  }
+  
+  file { '/etc/exports':
+    content => "${nfs_srv_folder} ${::network_br_storage}/255.255.255.0(rw,sync,no_subtree_check)",
+  }
+  
+  exec { 'exportfs -ra':
+    path        => ['/usr/bin', '/usr/sbin'],
+    subscribe   => File['/etc/exports'],
+    refreshonly => true
+  }
+  
+  service { $services_name:
+    ensure => running,
+  }
+}
+else {
+  fail("Unsuported osfamily ${::osfamily}, currently Debian are the only supported platforms")
+}
